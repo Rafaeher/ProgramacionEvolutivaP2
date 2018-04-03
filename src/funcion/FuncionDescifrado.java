370004eb1f754f60c4f95456b83f3c3d7f97b046
Quiremedas <quiremedas@gmail.com>
2018-04-03 15:40:28 +0200

    Commit

--- src/funcion/FuncionDescifrado.java
+++ src/funcion/FuncionDescifrado.java
@@ -3,22 +3,23 @@ package funcion;
 import java.util.ArrayList;
 
 import configuracion.Configuracion;
+import fenotipo.FenotipoMensaje;
 import fenotipo.FenotipoReal;
 import fitness.FitnessReal;
 import genotipo.Genotipo;
 import individuo.Individuo;
 
-public class FuncionDescifrado<GenotipoFD extends Genotipo> extends Funcion<GenotipoFD, FenotipoReal, FitnessReal>
+public class FuncionDescifrado<GenotipoFD extends Genotipo> extends Funcion<GenotipoFD, FenotipoMensaje, FitnessReal>
 {
 
 	public FuncionDescifrado
-	(ArrayList<Individuo<GenotipoFD, FenotipoReal, FitnessReal>> poblacion, Configuracion configuracion)
+	(ArrayList<Individuo<GenotipoFD, FenotipoMensaje, FitnessReal>> poblacion, Configuracion configuracion)
 	{
 		super(poblacion, configuracion);
 	}
 
 	@Override
-	public void algEvalua(ArrayList<Individuo<GenotipoFD, FenotipoReal, FitnessReal>> poblacion)
+	public void algEvalua(ArrayList<Individuo<GenotipoFD, FenotipoMensaje, FitnessReal>> poblacion)
 	{
 		//TODO
 	}
