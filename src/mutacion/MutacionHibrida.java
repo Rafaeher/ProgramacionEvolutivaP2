54b78e52e6a159bdaf42b4aeb80bc7d41bbb649c
charliee97 <carlosgcereceda@hotmail.es>
2018-04-22 19:26:39 +0200

    Nueva mutacion

--- src/mutacion/MutacionHibrida.java
+++ src/mutacion/MutacionHibrida.java
@@ -11,73 +11,91 @@ import fitness.Fitness;
 import genotipo.GenotipoAlfabeto;
 
 public class MutacionHibrida<FenotipoUPB extends Fenotipo, FitnessUPB extends Fitness>
-implements Mutacion<GenotipoAlfabeto, FenotipoMensaje, FitnessUPB>  {
+		implements Mutacion<GenotipoAlfabeto, FenotipoMensaje, FitnessUPB> {
 
 	@Override
 	public void muta(GenotipoAlfabeto genotipo, FenotipoMensaje fenotipo, double prob_mutacion) {
-		Random r = new Random();
-		
-			HashMap<String, Double> mapTop1 = DAO_Ngramas.lectura(6);
-			HashMap<String, Double> mapTop2 = DAO_Ngramas.lectura(7);
-			HashMap<String, Double> mapTop3 = DAO_Ngramas.lectura(8);
-			HashMap<String, Double> mapTop4 = DAO_Ngramas.lectura(9);
-			HashMap<String, Double> mapTop5 = DAO_Ngramas.lectura(10);
-			
-			HashMap<String, Double> mapAux = new HashMap<String, Double>();
-			
-			int top1 = 0, top2 = 0;
-			
-			String[] palabras = fenotipo.getMensajeDecodificadoLowerCase().split(" ");
-			HashMap<Character, Double> aux = new HashMap<Character, Double>();
-			for(int i = 0; i < palabras.length; i++){
-				if((mapTop1.containsKey(palabras[i]) || mapTop2.containsKey(palabras[i]))
-						&& !mapAux.containsKey(palabras[i])){
-					mapAux.put(palabras[i], null);
-					if(mapTop1.containsKey(palabras[i])) top1 += 1;
-					if(mapTop2.containsKey(palabras[i])) top2 += 1;
-					for(int j = 0; j < palabras[i].length(); j++){
-						if(!aux.containsKey(palabras[i].charAt(j))){
-							aux.put(palabras[i].charAt(j), null);
-						}
-					}
+		HashMap<String, Double> ngramas = DAO_Ngramas.lectura(1);
+		HashMap<String, Double> frecuenciaText = new HashMap<String, Double>();
+		String mensaje = fenotipo.getMensajeDecodificadoLowerCase();
+	//	System.out.println("TAMAÑO " + fenotipo.getMensajeDecodificadoLowerCase().length());
+		for(int i = 0; i < mensaje.length(); ++i){
+			if(mensaje.charAt(i) != ' ' && esLetra(mensaje.charAt(i))){
+				if(!frecuenciaText.containsKey(mensaje.charAt(i))){
+					frecuenciaText.put(Character.toString(mensaje.charAt(i)), 1.0 / mensaje.length());
+				}
+				else{
+					double veces = (frecuenciaText.get(Character.toString(mensaje.charAt(i))) * 
+							mensaje.length()) + 1.0;
+					double frecuencia = veces / mensaje.length();
+					frecuenciaText.put(Character.toString(mensaje.charAt(i)), frecuencia);
 				}
 			}
-			int a = 0, b = 0, num = 0, i = 0;
-			boolean ok = false;
-			r = new Random();
-			
-			double posibilidad = (top1 * 0.3) + (top2 * 0.1);
-			//if(top1 == 0) posibilidad = 0.0;
-			
-			for(int j = 0; j < genotipo.getCodigo().size(); j++){
-				r = new Random();
-				if (r.nextDouble() < prob_mutacion) {
-					while(!ok){
-						i = r.nextInt(genotipo.getSize());
-						if(genotipo.getCodigo().get(i) == 'h' && aux.containsKey(genotipo.getCodigo().get(i))){
-							System.out.println("h estaba");
-						}
-						if((!aux.containsKey(genotipo.getCodigo().get(i)) || r.nextDouble() > posibilidad) && num == 0){
-							num += 1;
-							a = i;
-						}
-						i = r.nextInt(genotipo.getSize());
-						if((!aux.containsKey(genotipo.getCodigo().get(i)) || r.nextDouble() > posibilidad) && num == 1){
-							num += 1;
-							b = i;
-							ok = true;
-						}
-					}
-				}
-				ArrayList<Character> array = genotipo.getCodigo();
-				
-				Character characterAux = array.get(a);
-				
-				array.set(a, array.get(b));
-				array.set(b, characterAux);
-
-				genotipo.setCodigo(array);
-			}
-			
 		}
+		//YA HEMOS CALCULADO LA FRECUENCIA DE LOS MONOGRAMAS
+		//AHORA INTERCAMBIAREMOS LOS DOS CON FRECUENCIA MAS BAJA
+		double errorMasAlto1 = 0.0;
+		String errorMasAlto1Character = "";
+		double errorMasAlto2 = 0.0;
+		String errorMasAlto2Character = "";
+		for (String ngrama : frecuenciaText.keySet()) {
+			double error = Math.abs(ngramas.get(ngrama) - frecuenciaText.get(ngrama));
+
+			
+			
+			if(error > errorMasAlto2 && error <= errorMasAlto1){
+				errorMasAlto2 = error;
+				errorMasAlto2Character = ngrama;
+			}
+			if(error > errorMasAlto2 && error > errorMasAlto1){
+				String characterAux = errorMasAlto1Character;
+				double errorAux = errorMasAlto1;
+				errorMasAlto1Character = ngrama;
+				errorMasAlto1 = error;
+				errorMasAlto2 = errorAux;
+				errorMasAlto2Character = characterAux;
+			}
+		}
+		
+		ArrayList<Character> array = genotipo.getCodigo();
+		for(int i = 0; i < array.size(); i++){
+			if(array.get(i) == errorMasAlto1Character.charAt(0)){
+				array.set(i, errorMasAlto2Character.charAt(0));
+			}
+			if(array.get(i) == errorMasAlto2Character.charAt(0)){
+				array.set(i, errorMasAlto1Character.charAt(0));
+			}
+		}
+		
+			
+	}
+	private boolean esLetra(char c)
+	{
+		return c == 'a' ||
+				c == 'b' ||
+				c == 'c' ||
+				c == 'd' ||
+				c == 'e' ||
+				c == 'f' ||
+				c == 'g' ||
+				c == 'h' ||
+				c == 'i' ||
+				c == 'j' ||
+				c == 'k' ||
+				c == 'l' ||
+				c == 'm' ||
+				c == 'n' ||
+				c == 'o' ||
+				c == 'p' ||
+				c == 'q' ||
+				c == 'r' ||
+				c == 's' ||
+				c == 't' ||
+				c == 'u' ||
+				c == 'v' ||
+				c == 'w' ||
+				c == 'x' ||
+				c == 'y' ||
+				c == 'z';
+	}
 }
